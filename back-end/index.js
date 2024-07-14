45ac317a9977841508c773f09fda0b10fccfd168
Tshepho <www.tshepodavid@gmail.com>
2024-07-14 14:27:59 +0200

    first commit

--- back-end/index.js
+++ back-end/index.js
@@ -1,35 +1,37 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const app = express()
-const product = require ('./routes/Product')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const product = require('./routes/Product');
 const bodyParser = require('body-parser');
 const userRoute = require('./routes/User');
-const order = require('./routes/Order')
+const order = require('./routes/Order');
 
-mongoose
-  .connect(process.env.MONGODB_ATLAS_URI || "mongodb+srv://tshepho:davidrap@cluster0.dwyuahg.mongodb.net/Marketing")
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
-
-app.use(cors(
-  {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_ATLAS_URI || "mongodb+srv://tshepho:davidrap@cluster0.dwyuahg.mongodb.net/Marketing", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit process with failure
   }
-));
+};
+
+connectDB();
+
+app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json())
-app.use('/product', product)
+app.use(express.json());
+app.use('/product', product);
 app.use('/user', userRoute);
-app.use('/order',order);
+app.use('/order', order);
 
 app.use(express.urlencoded({ extended: false }));
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log('app is on Port ' + port)
-})
-
+  console.log(`App is running on port ${port}`);
+});
