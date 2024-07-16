9f8c05f7ff51f552b8d59a0fc4305daf7b3ba7b2
Tshephodave <111848064+Tshephodave@users.noreply.github.com>
2024-07-16 20:07:43 +0200

    Update User.js

--- back-end/model/User.js
+++ back-end/model/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  agent: { type: String, required: true,unique:true },
+  
   phone: {
     type: String,
     validate: {
@@ -13,42 +15,7 @@ const userSchema = new mongoose.Schema({
     },
     required: [true, 'User phone number required']
 },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Password cannot contain "password"')
-      }
-    }
-  },
-  passwordConfirm: {
-    type: String,
-    required: true,
-    minlength: [7, 'Password should have atleast 7 characters.'],
-    validate: {
-      validator: function (val) {
-        return toString(this.password) === toString(val)
-      },
-      message: 'Provided passwords do not match. Please try again'
-    }
-  },
-  passwordChangedAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  passwordResetToken: {
-    type: String,
-    select: false
-  },
-  passwordResetValidity: {
-    type: Date,
-    select: false
-  },
-
+  
   address:{type: String, required: true},
   role: { type: String, enum: ['customer', 'admin'], required: true },
 });
