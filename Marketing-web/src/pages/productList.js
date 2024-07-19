883216829943bc96aee43c9a315caf679034cded
Tshephodave <111848064+Tshephodave@users.noreply.github.com>
2024-07-19 11:52:28 +0200

    Update productList.js

--- Marketing-web/src/pages/productList.js
+++ Marketing-web/src/pages/productList.js
@@ -128,7 +128,7 @@ const ProductList = () => {
         {products.map(product => (
           <div key={product._id} className="bg-white p-4 rounded-lg shadow-md">
             <img src={product.picture} alt={product.name} className="h-64 w-full object-cover mb-4 rounded-t-lg" />
-            <p className="text-red-600">{product.itemCode}</p>
+            <p className="text-red-600"><strong>ItemCode:</strong>{product.itemCode}</p>
             <h2 className="text-xl font-semibold">{product.name}</h2>
             <p className="text-gray-600">{product.description}</p>
             <p className="text-lg font-bold mt-2">R{product.price}</p>
