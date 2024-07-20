b740b585bd6fc3225e5441e6ae451d236382978b
Tshephodave <111848064+Tshephodave@users.noreply.github.com>
2024-07-20 14:00:10 +0200

    Update productList.js

--- Marketing-web/src/pages/productList.js
+++ Marketing-web/src/pages/productList.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { CartContext } from '../context/cartContext';
 import { jwtDecode } from 'jwt-decode';
-import Loading from './components/Loading'; 
+import Loading from '../components/Loading'; 
 
 import Modal from 'react-modal';
 
