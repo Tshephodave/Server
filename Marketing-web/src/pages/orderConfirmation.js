346c7a1431f7abcb32cb2b19e5473f9532446618
Tshephodave <111848064+Tshephodave@users.noreply.github.com>
2024-07-20 14:00:30 +0200

    Update orderConfirmation.js

--- Marketing-web/src/pages/orderConfirmation.js
+++ Marketing-web/src/pages/orderConfirmation.js
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Loading from './components/Loading'; 
+import Loading from '../components/Loading'; 
 
 const OrderConfirmation = () => {
   const [orders, setOrders] = useState([]);
