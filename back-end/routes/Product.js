009c03968008a698e52dddbc0f3937788f957c88
Tshephodave <111848064+Tshephodave@users.noreply.github.com>
2024-07-25 21:42:39 +0200

    Update Product.js

--- back-end/routes/Product.js
+++ back-end/routes/Product.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {
- addProduct,getProducts,deleteProduct, updateProduct
+ addProduct,getProducts,deleteProduct, updateProduct,searchProducts
 } = require('../controller/productController');
 const {authenticateUser} = require('../middleware/auth');
 router.post('/addProduct',authenticateUser,  addProduct);
 router.get('/getProducts', getProducts);
 router.delete('/deleteProduct/:id', deleteProduct);
 router.put('/updateProduct/:id', updateProduct);
+router.get('/searchProducts',searchProducts );
+
 
 
 module.exports = router;
