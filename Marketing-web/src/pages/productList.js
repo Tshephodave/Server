6982b9bd4c35fbf0fdf0bbb831b18f44dddce74c
Tshephodave <111848064+Tshephodave@users.noreply.github.com>
2024-08-07 09:03:57 +0200

    Update productList.js

--- Marketing-web/src/pages/productList.js
+++ Marketing-web/src/pages/productList.js
@@ -289,7 +289,7 @@ const ProductList = () => {
   isOpen={noProductsFound}
   onRequestClose={() => setNoProductsFound(false)}
   contentLabel="No Products Found"
-  className="fixed inset-0 flex items-center justify-center z-50"
+  className="fixed inset-0 flex items-center justify-center z-50 mt-[20px]"
   overlayClassName="fixed inset-0 bg-black bg-opacity-50"
 >
   <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md mx-auto">
