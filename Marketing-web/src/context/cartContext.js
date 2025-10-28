e0e7d7a944ac0b4a63a078769ed5f1e957ba9312
Tshephodave <111848064+Tshephodave@users.noreply.github.com>
2025-10-28 09:22:47 +0200

    Update cartContext.js

--- Marketing-web/src/context/cartContext.js
+++ Marketing-web/src/context/cartContext.js
@@ -18,13 +18,14 @@ export const CartProvider = ({ children }) => {
     });
   };
 
-  const removeFromCart = (productId) => {
+  const decreaseQuantity = (productId) => {
     setCart((prevCart) =>
       prevCart.reduce((acc, item) => {
         if (item.product._id === productId) {
           if (item.quantity > 1) {
             acc.push({ ...item, quantity: item.quantity - 1 });
           }
+          // If quantity <= 1, don't push the item (effectively removing it)
         } else {
           acc.push(item);
         }
@@ -33,12 +34,16 @@ export const CartProvider = ({ children }) => {
     );
   };
 
+  const removeFromCart = (productId) => {
+    setCart(prevCart => prevCart.filter(item => item.product._id !== productId));
+  };
+
   const clearCart = () => {
     setCart([]);
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, decreaseQuantity }}>
       {children}
     </CartContext.Provider>
   );
