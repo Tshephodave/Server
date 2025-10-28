fdd33befd6045fa90ca5edff819904f0143e8ab0
Tshephodave <111848064+Tshephodave@users.noreply.github.com>
2025-10-28 09:21:39 +0200

    Update PrivateRoute.js

--- Marketing-web/src/components/PrivateRoute.js
+++ Marketing-web/src/components/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ user, children }) => {
   if (!user) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return children;
